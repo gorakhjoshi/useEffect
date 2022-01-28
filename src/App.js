23973e6528f9b31a26ea7a60a26eae4624384ab5
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-28 19:07:44 +0545

    pokemon form UI added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
-  return <div>Initial</div>;
+  return (
+    <div className='pokemon-info-app'>
+      <form className='pokemon-form'>
+        {console.log('Rendering pokemon Form UI')}
+        <label htmlFor='pokemonName-input'>Pokemon Name</label>
+        <small>
+          Try{' '}
+          <button className='invisible-button' type='button'>
+            "pikachu"
+          </button>
+          {', '}
+          <button className='invisible-button' type='button'>
+            "charizard"
+          </button>
+          {', or '}
+          <button className='invisible-button' type='button'>
+            "mew"
+          </button>
+        </small>
+        <div>
+          <input
+            className='pokemonName-input'
+            id='pokemonName-input'
+            name='pokemonName'
+            placeholder='Pokemon Name...'
+          />
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default App;
