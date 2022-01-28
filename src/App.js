0ed4912ed71a018035fe32785020ac736056225a
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-28 19:17:42 +0545

    added state to pokemon form

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+  const [pokemonName, setPokemonName] = React.useState('');
+
+  console.log(pokemonName);
+
+  function handleChange(e) {
+    setPokemonName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setPokemonName(pokemonName);
+  }
+
+  function handleSelect(newPokemonName) {
+    setPokemonName(newPokemonName);
+  }
   return (
     <div className='pokemon-info-app'>
-      <form className='pokemon-form'>
-        {console.log('Rendering pokemon Form UI')}
+      <form onSubmit={handleSubmit} className='pokemon-form'>
         <label htmlFor='pokemonName-input'>Pokemon Name</label>
         <small>
           Try{' '}
-          <button className='invisible-button' type='button'>
+          <button
+            className='invisible-button'
+            type='button'
+            onClick={() => handleSelect('pikachu')}
+          >
             "pikachu"
           </button>
           {', '}
-          <button className='invisible-button' type='button'>
+          <button
+            className='invisible-button'
+            type='button'
+            onClick={() => handleSelect('charizard')}
+          >
             "charizard"
           </button>
           {', or '}
-          <button className='invisible-button' type='button'>
+          <button
+            className='invisible-button'
+            type='button'
+            onClick={() => handleSelect('mew')}
+          >
             "mew"
           </button>
         </small>
@@ -27,8 +55,12 @@ const App = () => {
             id='pokemonName-input'
             name='pokemonName'
             placeholder='Pokemon Name...'
+            value={pokemonName}
+            onChange={handleChange}
           />
-          <button type='submit'>Submit</button>
+          <button type='submit' disabled={!pokemonName.length}>
+            Submit
+          </button>
         </div>
       </form>
     </div>
